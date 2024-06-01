ab4c9c67e6753369e90237bd7e770c272bbbf0c6
Valdo Mpinga <valdompinga2@gmail.com>
2024-06-01 19:45:56 +0100

    polygon area done

--- index.js
+++ index.js
@@ -142,6 +142,10 @@ function startDrawing(index)
                     if (polygon === null)
                     {
                         polygon = L.polygon([e.latlng], { color: category.color }).addTo(map);
+                        polygon.on('click', function ()
+                        {
+                            alert('Polygon area: ' + calculatePolygonArea(polygon));
+                        });
                         category.layers.push(polygon);
                     } else
                     {
@@ -183,5 +187,37 @@ function calculatePolylineLength(polyline)
 
     return totalLength.toFixed(2) + ' meters';
 }
+
+function calculatePolygonArea(polygon)
+{
+    var latlngs = polygon.getLatLngs()[0]; // get the array of latlngs
+    var area = L.GeometryUtil.geodesicArea(latlngs);
+    return (area / 1000000).toFixed(2) + ' square kilometers'; // convert to square kilometers
+}
+
+// Load GeometryUtil library for area calculations
+L.GeometryUtil = L.extend(L.GeometryUtil || {}, {
+    geodesicArea: function (latlngs)
+    {
+        var pointsCount = latlngs.length,
+            area = 0.0,
+            d2r = Math.PI / 180,
+            p1, p2;
+
+        if (pointsCount > 2)
+        {
+            for (var i = 0; i < pointsCount; i++)
+            {
+                p1 = latlngs[i];
+                p2 = latlngs[(i + 1) % pointsCount];
+                area += ((p2.lng - p1.lng) * d2r) *
+                    (2 + Math.sin(p1.lat * d2r) + Math.sin(p2.lat * d2r));
+            }
+            area = area * 6378137.0 * 6378137.0 / 2.0;
+        }
+
+        return Math.abs(area);
+    }
+});
 
 renderCategories();
